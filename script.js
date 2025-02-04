f9acd86332049aa8dc024683055249473127f876
Vaish0711 <vaishnavidahane2000@gmail.com>
2025-02-04 22:36:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,9 +2,8 @@ function firstWord(s) {
   // your code here
 	if(!s) return '';
 	s=s.trim();
-	const firstSpaceIndex = str.indexOf(' ');
-    return firstSpaceIndex === -1 ? str : str.slice(0, firstSpaceIndex);
-
+	const firstSpaceIndex = s.indexOf(' ');
+    return firstSpaceIndex === -1 ? s : s.slice(0, firstSpaceIndex);
 }
 
 // Do not change the code below
